0cafc0b84d42d0754ec2ffb0aac4d63771455ddd
Julian Waller <git@julusian.co.uk>
2017-04-09 11:01:06 +0100

    Move js file selection to server.js using a template engine

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 const express = require('express');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
 
 import { webui_port } from "./config";
 
@@ -19,9 +20,17 @@ const server = app.listen(webui_port, () => {
 
 const io = require('socket.io')(server);
 
+// Setup nunjucks templating engine
+const env = nunjucks.configure(app.get('views'), {
+    noCache: true,
+    watch: true,
+    express: app
+});
+
 app.use(bodyParser.urlencoded({ extended: false } ));
 app.use(bodyParser.json());
 app.use(express.static('static'));
+app.engine( 'html', nunjucks.render );
 
 // queueSetup(Models, app, io);
 sceneSetup(Models, app);
@@ -38,10 +47,9 @@ io.sockets.on('connection', (socket) => {
   // queueBind(Models, socket);
 });
 
+const scriptSrc = (process.env.NODE_ENV == "production") ? "app.js" : "http://localhost:8087/static/app.js";
+
 // Set Express routes.
 app.get('/', (req, res) => {
-  if(process.env.NODE_ENV == "production")
-    res.sendFile(__dirname + '/views/index.html');
-  else
-    res.sendFile(__dirname + '/views/dev.html');
+  res.render(__dirname + '/views/index.html', { scriptSrc: scriptSrc });
 });
